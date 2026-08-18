94dc8a8b9aa9ffb7f84f63068b0e44a0e8e51fe0
kabakaro12 <61512272+kabakaro12@users.noreply.github.com>
2026-08-18 02:07:26 +0200

    Enable cat provider

--- app/[locale]/layout.js
+++ app/[locale]/layout.js
@@ -1,7 +1,9 @@
+import { CartProvider } from "../../components/CartProvider";
+
 export function generateStaticParams() {
   return [{ locale: "fr" }, { locale: "en" }];
 }
 
 export default function LocaleLayout({ children }) {
-  return children;
+  return <CartProvider>{children}</CartProvider>;
 }
